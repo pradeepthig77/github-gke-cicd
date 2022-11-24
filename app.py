132a01d93772bdb5ad8479a06cc33ef8feaf0fed
pradeepthig77 <87798584+pradeepthig77@users.noreply.github.com>
2022-11-24 12:47:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask('hello-world')
+@app.route('/')
+def hello():
+ return "Hello world!\n"
+if __name__ == '__main__':
+ app.run(host = '0.0.0.0', port = 8080)
+# [END hello-app
